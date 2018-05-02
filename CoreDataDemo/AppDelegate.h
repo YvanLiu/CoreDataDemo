442faf7737a47ab9c072665bb46ec0685d2b112f
YvanLiu <13370124404@163.com>
2018-05-02 14:35:45 +0800

    Initial Commit

---
+++ CoreDataDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataDemo
+//
+//  Created by sjimac01 on 2018/5/2.
+//  Copyright © 2018年 sjimac01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataDemo
+//
+//  Created by sjimac01 on 2018/5/2.
+//  Copyright © 2018年 sjimac01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
